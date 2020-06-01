fa5261340c9f6dd0353b6f3e162c97b0e826c84d
Guohui Xiao <xiao@inf.unibz.it>
2020-06-01 23:04:47 +0200

    download page

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,246 +1,250 @@
 module.exports = {
-	base: '/',
-	title: 'Ontop',
-	themeConfig: {
-		repo: 'ontop/ontop',
-		docsRepo: 'ontop/ontop-website',
-		editLinks: true,
-		lastUpdated: 'Last Updated',
-		nav: [
-			{
-			  text: 'Guide',
-			  link: '/guide/',
-			},
-			{
-				text: 'Tutorial',
-				link: '/tutorial/',
-			},
-			{
-				text: 'Community',
-				link: '/community/'
-			},
-			{
-				text: 'Research',
-				link: '/research/'
-			},
-			{
-				text: 'Dev',
-				link: '/dev/'
-			},
-		],
-		sidebar: {
-			'/guide/': genGuideSidebarConfig(),
-			'/tutorial/': genTutorialSidebarConfig(),
-			'/dev/': genDevSidebarConfig(),
-			'/community/': genCommunitySidebarConfig(),
-			'/research/': genResearchSidebarConfig(),
-		}
-	},
-	plugins: [
-		['mathjax', {
-		  target: 'svg',
-		  macros: {
-			'*': '\\times',
-		  },
-		}],
-		["vuepress-plugin-matomo",
-			{
-			  'siteId': 2,
-			  'trackerUrl': "https://ontopic.matomo.cloud/"
-			}
-		  ],
-		  ["redirect", {
-				redirectors: [
-				  {
-					base: '/obda-pages/ontop',
-					storage: false, 
-					alternative: [
-					  '../..',
-					],
-				  },
-				],
-			  },
-		  ]
-	  ]
+    base: '/',
+    title: 'Ontop',
+    themeConfig: {
+        repo: 'ontop/ontop',
+        docsRepo: 'ontop/ontop-website',
+        editLinks: true,
+        lastUpdated: 'Last Updated',
+        nav: [
+            {
+                text: 'Guide',
+                link: '/guide/',
+            },
+            {
+                text: 'Tutorial',
+                link: '/tutorial/',
+            },
+            {
+                text: 'Community',
+                link: '/community/'
+            },
+            {
+                text: 'Research',
+                link: '/research/'
+            },
+            {
+                text: 'Dev',
+                link: '/dev/'
+            },
+            {
+                text: 'Download',
+                link: '/download/'
+            }
+        ],
+        sidebar: {
+            '/guide/': genGuideSidebarConfig(),
+            '/tutorial/': genTutorialSidebarConfig(),
+            '/dev/': genDevSidebarConfig(),
+            '/community/': genCommunitySidebarConfig(),
+            '/research/': genResearchSidebarConfig(),
+        }
+    },
+    plugins: [
+        ['mathjax', {
+            target: 'svg',
+            macros: {
+                '*': '\\times',
+            },
+        }],
+        ["vuepress-plugin-matomo",
+            {
+                'siteId': 2,
+                'trackerUrl': "https://ontopic.matomo.cloud/"
+            }
+        ],
+        ["redirect", {
+            redirectors: [
+                {
+                    base: '/obda-pages/ontop',
+                    storage: false,
+                    alternative: [
+                        '../..',
+                    ],
+                },
+            ],
+        },
+        ]
+    ]
 }
 
-function genGuideSidebarConfig () {
-	return [
-	  {
-		title: 'Guide',
-		collapsable: false,
-		children: [
-		  '',
-		  'concepts',
-		  'getting-started',
-		  'cli'
-		]
-	  },
-	//   {
-	// 	title: 'Advanced',
-	// 	collapsable: false,
-	// 	children: [
-	// 	]
-	//   },
-	  {
-		title: 'Troubleshooting',
-		collapsable: false,
-		children: [
-			'troubleshooting/faq'
-		]
-	  },
-	  {
-		title: 'Meta',
-		collapsable: false,
-		children: [
-		  'glossary',
-		  'releases'
-		]
-	  }
-	]
-  }
+function genGuideSidebarConfig() {
+    return [
+        {
+            title: 'Guide',
+            collapsable: false,
+            children: [
+                '',
+                'concepts',
+                'getting-started',
+                'cli'
+            ]
+        },
+        //   {
+        // 	title: 'Advanced',
+        // 	collapsable: false,
+        // 	children: [
+        // 	]
+        //   },
+        {
+            title: 'Troubleshooting',
+            collapsable: false,
+            children: [
+                'troubleshooting/faq'
+            ]
+        },
+        {
+            title: 'Meta',
+            collapsable: false,
+            children: [
+                'glossary',
+                'releases'
+            ]
+        }
+    ]
+}
 
-function genDevSidebarConfig () {
-	return [
-	  {
-		title: 'Development',
-		collapsable: false,
-		children: [
-		  '',
-		  'git',
-		  'build',
-		  'new-release',
-		  'debug-protege',
-		  'debug-jetty',
-		  'db-adapter'
-		]
-	  },
-	  {
-		title: 'Internals',
-		collapsable: false,
-		children: [
-		  'internals/iq',
-		]
-	  },
-	  {
-		  title: 'Outdated corner',
-		  children: [
-			'outdated/known-issues',
-		  ]
-	  }
-	]
-  }
+function genDevSidebarConfig() {
+    return [
+        {
+            title: 'Development',
+            collapsable: false,
+            children: [
+                '',
+                'git',
+                'build',
+                'new-release',
+                'debug-protege',
+                'debug-jetty',
+                'db-adapter'
+            ]
+        },
+        {
+            title: 'Internals',
+            collapsable: false,
+            children: [
+                'internals/iq',
+            ]
+        },
+        {
+            title: 'Outdated corner',
+            children: [
+                'outdated/known-issues',
+            ]
+        }
+    ]
+}
 
-  function genTutorialSidebarConfig () {
-	return [
-	  {
-		title: 'Tutorial',
-		collapsable: false,
-		children: [
-		  '',
-		]
-	  },
-	  {
-		title: 'Basics',
-		collapsable: false,
-		children: [
-		  'basic/setup',
-		  'basic/university-1',
-		  'basic/university-2',
-		]
-	  },
-	  {
-		title: 'Endpoint',
-		collapsable: false,
-		children: [
-			'endpoint/',
-		  'endpoint/endpoint-cli',
-		  'endpoint/endpoint-docker',
-		  //'endpoint/endpoint-tomcat',
-		]
-	  },
-	  {
-		title: 'Interact',
-		collapsable: false,
-		children: [
-			'interact/cli.md',
-			'interact/jupyter.md',
-		]
-	  },
-	  {
-		title: 'Mapping',
-		collapsable: false,
-		children: [
-			'mapping/',
-		  'mapping/primary-keys',
-		  'mapping/foreign-keys',
-		  'mapping/uri-templates',
-		  'mapping/existential',
-		]
-	  },
-	  {
-		title: 'Federation',
-		collapsable: false,
-		children: [
-			'federation/',
-		  'federation/denodo/',
-		  'federation/dremio/',
-		]
-	  },
-	  /*{
-		title: 'Api',
-		collapsable: false,
-		children: [
-			'api/',
-		]
-	  }*/
+function genTutorialSidebarConfig() {
+    return [
+        {
+            title: 'Tutorial',
+            collapsable: false,
+            children: [
+                '',
+            ]
+        },
+        {
+            title: 'Basics',
+            collapsable: false,
+            children: [
+                'basic/setup',
+                'basic/university-1',
+                'basic/university-2',
+            ]
+        },
+        {
+            title: 'Endpoint',
+            collapsable: false,
+            children: [
+                'endpoint/',
+                'endpoint/endpoint-cli',
+                'endpoint/endpoint-docker',
+                //'endpoint/endpoint-tomcat',
+            ]
+        },
+        {
+            title: 'Interact',
+            collapsable: false,
+            children: [
+                'interact/cli.md',
+                'interact/jupyter.md',
+            ]
+        },
+        {
+            title: 'Mapping',
+            collapsable: false,
+            children: [
+                'mapping/',
+                'mapping/primary-keys',
+                'mapping/foreign-keys',
+                'mapping/uri-templates',
+                'mapping/existential',
+            ]
+        },
+        {
+            title: 'Federation',
+            collapsable: false,
+            children: [
+                'federation/',
+                'federation/denodo/',
+                'federation/dremio/',
+            ]
+        },
+        /*{
+          title: 'Api',
+          collapsable: false,
+          children: [
+              'api/',
+          ]
+        }*/
 
-	]
-  }
+    ]
+}
 
-function genCommunitySidebarConfig () {
-	return [
-	  {
-		title: 'Community',
-		collapsable: false,
-		children: [
-		  '',
-		  'organizations',
-		  'support',
-		]
-	  },
-	  {
-		title: 'Contributing',
-		collapsable: false,
-		children: [
-			'contributing/',
-			'contributing/bug-report',
-			'contributing/pull-request',
-		  'contributing/documentation'
-		]
-	  }
-	]
-  }
+function genCommunitySidebarConfig() {
+    return [
+        {
+            title: 'Community',
+            collapsable: false,
+            children: [
+                '',
+                'organizations',
+                'support',
+            ]
+        },
+        {
+            title: 'Contributing',
+            collapsable: false,
+            children: [
+                'contributing/',
+                'contributing/bug-report',
+                'contributing/pull-request',
+                'contributing/documentation'
+            ]
+        }
+    ]
+}
 
 
-function genResearchSidebarConfig () {
-	return [
-	  {
-		title: 'Research',
-		collapsable: false,
-		children: [
-		  '',
-		  'publications',
-		]
-	  },
-	  {
-		title: 'Theory',
-		collapsable: false,
-		children: [
-		  'notation',
-		  'iq-formal',
-		  'optimization',
-		]
-	  }
-	]
-  }
+function genResearchSidebarConfig() {
+    return [
+        {
+            title: 'Research',
+            collapsable: false,
+            children: [
+                '',
+                'publications',
+            ]
+        },
+        {
+            title: 'Theory',
+            collapsable: false,
+            children: [
+                'notation',
+                'iq-formal',
+                'optimization',
+            ]
+        }
+    ]
+}
---
+++ download/README.md
@@ -0,0 +1,25 @@
+# Download
+
+## Ready-to-use Binaries
+
+Ready-to-use binaries are published on [Sourceforge](https://sourceforge.net/projects/ontop4obda/files/) and on [Github releases](https://github.com/ontop/ontop/releases), 
+with the following files:
+
+- `ontop-cli-[version].zip`: Command line interface of Ontop.
+- `it.unibz.inf.ontop.protege-[version].jar`: plugin for Protege 5.x installation files (drop it in Protege’s plugins folder).
+- `ontop-protege-bundle-[version].zip` : Protege bundled with Ontop. This is ready to run package, unzip and use the run.sh or run.bat start scripts.
+- `ontop-webapps-[version].zip`: WAR files required to create an Ontop SPARQL end-point webapp. Drop this into the webapps folder of your Servlet server.
+- `ontop-tomcat-bundle-[version].zip`: Tomcat + RDF4J workbench + Ontop ready to run a SPARQL endpoint.
+- `ontop-jetty-bundle-[version].zip`: Jetty + RDF4J workbench + Ontop ready to run a SPARQL endpoint.
+
+
+## Docker Image
+
+ A Docker image of the
+SPARQL endpoint is available at [Docker Hub](https://hub.docker.com/r/ontop/ontop-endpoint).
+
+## Protege Plugin
+
+The Ontop plugin for Protege is available also in the plugin repository of
+Protege, through which users receive auto-updates. 
+
