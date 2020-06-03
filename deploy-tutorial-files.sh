091e73407baf99b60510724398a6a7b64aa2c56e
Benjamin Cogrel <benjamin.cogrel@ontopic.biz>
2020-06-03 13:28:54 +0200

    GITHUB_TOKEN added to the script.

--- deploy-tutorial-files.sh
+++ deploy-tutorial-files.sh
@@ -4,6 +4,6 @@ cd .vuepress/public/tutorial
 git init
 git add *
 git commit "Tutorial files extracted"
-git remote add origin https://github.com/ontop/ontop-tutorial.git
+git remote add origin https://ontop-travis:${GITHUB_TOKEN}@github.com/ontop/ontop-tutorial.git
 git push -f origin master
 rm -rf .git
